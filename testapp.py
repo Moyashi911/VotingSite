82d0aee1317dca4e7c1963974e1a1eb47ee25f8f
Moyashi911 <139599490+Moyashi911@users.noreply.github.com>
2023-11-19 22:52:22 +0900

    Update testapp.py

--- testapp.py
+++ testapp.py
@@ -1,7 +1,6 @@
 from flask import Flask
-app = Flask(__name__, static_folder='.', static_url_path='')
-@app.route('/')
-def index():
-    return app.send_static_file('index.html')
+app = Flask(__name__)
 
-app.run()
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
